5ac6b324e7c9945053a7b85a3823beb426b4f5df
IvanB <Ivan.Bhargava@iiitb.ac.in>
2025-06-18 15:59:40 +0530

    2

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,15 @@
+// cms/config/server.js
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // read APP_KEYS from the environment, comma-separated
+    keys: env.array('APP_KEYS', [
+      'fallbackKey1',
+      'fallbackKey2',
+      'fallbackKey3',
+      'fallbackKey4',
+    ]),
   },
 });
