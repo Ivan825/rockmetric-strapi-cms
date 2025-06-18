0844c593b8f51592a55e9edaa4239c039150960b
IvanB <Ivan.Bhargava@iiitb.ac.in>
2025-06-18 16:06:39 +0530

    3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default ({ env }) => ({
+  // other plugin configs…
+
+  'users-permissions': {
+    config: {
+      // read your secret from the environment
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+});
